af6b66d1001425c3220d1e89787cb3d5cf2f5dc2
Ken Schlobohm <keschlob@microsoft.com>
2023-05-03 18:38:57 -0500

    fixing pwsh hook execution

--- infra/devOpsScripts/configureLoadTest.ps1
+++ infra/devOpsScripts/configureLoadTest.ps1
@@ -1,10 +1,19 @@
 ## TODO: Add header
 
+# Paths in this script are relative to the execution hooks specified in Azure.yml where this sccript will be invoked as part of the postdeploy process to start a load test after code has been deployed
+
 Write-Host 'Now building the tool'
-dotnet publish ./tools/Azure.LoadTest.Tool/Azure.LoadTest.Tool/Azure.LoadTest.Tool.csproj --output ./tools/Azure.LoadTest.Tool/publish
+$exitCodeForDotnetBuild = & "dotnet publish ../../tools/Azure.LoadTest.Tool/Azure.LoadTest.Tool/Azure.LoadTest.Tool.csproj --output ../../tools/Azure.LoadTest.Tool/publish"
+if ($exitCodeForDotnetBuild -ne 0) {
+    throw "Failed to build the load test tool"
+}
 
 $azdEnvironment = (azd env list --output json) | ConvertFrom-Json | Where-Object { $_.IsDefault -eq 'true' }
 Write-Host "Discovered AZD environment: ${azdEnvironment.name}"
 
 Write-Host 'Now running the Azure.LoadTest.Tool...'
-./tools/Azure.LoadTest.Tool/publish/Azure.LoadTest.Tool.exe --environment-name $azdEnvironment.name
+$exitCodeForLoadTestTool = & "../../tools/Azure.LoadTest.Tool/publish/Azure.LoadTest.Tool.exe --environment-name $azdEnvironment.name"
+
+if ($exitCodeForLoadTestTool -ne 0) {
+    throw "Failed to complete the load test configuration"
+}
