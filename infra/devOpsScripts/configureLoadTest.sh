d193053631110f5151b68300c310a96a34a03639
Ken Schlobohm <keschlob@microsoft.com>
2023-05-04 17:23:08 -0500

    fix hard coded environment

--- infra/devOpsScripts/configureLoadTest.sh
+++ infra/devOpsScripts/configureLoadTest.sh
@@ -11,11 +11,10 @@
 ################################################################################################
 
 echo 'Now building the tool'
-dotnet publish ./tools/Azure.LoadTest.Tool/Azure.LoadTest.Tool/Azure.LoadTest.Tool.csproj --output ./tools/Azure.LoadTest.Tool/publish
+#dotnet publish ./tools/Azure.LoadTest.Tool/Azure.LoadTest.Tool/Azure.LoadTest.Tool.csproj --output ./tools/Azure.LoadTest.Tool/publish
 
-# TODO: fix this hard coded value
-azdEnvironmentName=relekendev3
+azdEnvironmentName=$(azd env list | grep -w true | awk '{print $1}')
 echo "Discovered AZD environment: $azdEnvironmentName"
 
 echo 'Now running the Azure.LoadTest.Tool...'
-./tools/Azure.LoadTest.Tool/publish/Azure.LoadTest.Tool.exe --environment-name $azdEnvironmentName
+#./tools/Azure.LoadTest.Tool/publish/Azure.LoadTest.Tool.exe --environment-name $azdEnvironmentName
