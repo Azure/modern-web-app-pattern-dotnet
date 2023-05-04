2adf9d0dfb1258d38b46f29108ba6271d3b91fa1
Ken Schlobohm <keschlob@microsoft.com>
2023-05-04 09:20:32 -0500

    wrap console commands to report error

--- infra/devOpsScripts/configureLoadTest.ps1
+++ infra/devOpsScripts/configureLoadTest.ps1
@@ -3,16 +3,40 @@
 # Paths in this script are relative to the execution hooks specified in Azure.yml where this sccript will be invoked as part of the postdeploy process to start a load test after code has been deployed
 
 Write-Host 'Now building the tool'
-$exitCodeForDotnetBuild = & "dotnet publish ../../tools/Azure.LoadTest.Tool/Azure.LoadTest.Tool/Azure.LoadTest.Tool.csproj --output ../../tools/Azure.LoadTest.Tool/publish"
-if ($exitCodeForDotnetBuild -ne 0) {
-    throw "Failed to build the load test tool"
+
+try {
+    $pathToPublishFolder = "../../tools/Azure.LoadTest.Tool/publish"
+    $pathToCsProj = "../../tools/Azure.LoadTest.Tool/Azure.LoadTest.Tool/Azure.LoadTest.Tool.csproj"
+    $process = Start-Process dotnet -ArgumentList "publish --output $pathToPublishFolder $pathToCsProj" -Wait -NoNewWindow -PassThru -ErrorAction Stop
+
+    $process.WaitForExit()
+
+    if ($process.ExitCode -ne 0) {
+        throw "The build command exited with a non-zero code: $($process.ExitCode)"
+    }
+}
+catch {
+    throw "An error occurred while running the build command: $($_.Exception.Message)"
 }
 
 $azdEnvironment = (azd env list --output json) | ConvertFrom-Json | Where-Object { $_.IsDefault -eq 'true' }
-Write-Host "Discovered AZD environment: ${azdEnvironment.name}"
+Write-Host "Discovered AZD environment: $($azdEnvironment.name)"
 
 Write-Host 'Now running the Azure.LoadTest.Tool...'
-$exitCodeForLoadTestTool = & "../../tools/Azure.LoadTest.Tool/publish/Azure.LoadTest.Tool.exe --environment-name $azdEnvironment.name"
+
+try {
+    $pathToTool = "../../tools/Azure.LoadTest.Tool/publish/Azure.LoadTest.Tool.exe"
+    $process = Start-Process $pathToTool -ArgumentList "--environment-name $($azdEnvironment.name)" -Wait -NoNewWindow -PassThru -ErrorAction Stop
+
+    $process.WaitForExit()
+
+    if ($process.ExitCode -ne 0) {
+        throw "The load test tool app exited with a non-zero code: $($process.ExitCode)"
+    }
+}
+catch {
+    throw "An error occurred while running the load test tool app: $($_.Exception.Message)"
+}
 
 if ($exitCodeForLoadTestTool -ne 0) {
     throw "Failed to complete the load test configuration"
