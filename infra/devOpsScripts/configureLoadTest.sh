189ea35eb6b63adfc2f1e6fec8e44e167066d97d
John Poole <johnpoole@microsoft.com>
2023-05-22 12:56:52 -0400

    add bicep output for subscription; add exit code to alt bash script

--- infra/devOpsScripts/configureLoadTest.sh
+++ infra/devOpsScripts/configureLoadTest.sh
@@ -57,6 +57,7 @@ echo "#### END DEBUG"
 
 if [ $? -eq 0 ]; then
     echo "An error occurred while running the load test tool app. The file loadtest_tool.log has more details."
+    exit 1
 else
     echo "Command failed with exit code $?."
 fi
