f2b3447855e2829c6bd424b5f88f977d08f03929
Mike Rousos <mikerou@microsoft.com>
2023-12-19 15:29:10 -0500

    Add tests for options types and TicketRenderRequestEventHandler

--- tests/Relecloud.TicketRenderer.Tests/GlobalUsings.cs
+++ tests/Relecloud.TicketRenderer.Tests/GlobalUsings.cs
@@ -1,2 +1,8 @@
-global using Moq;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.Options;
+global using NSubstitute;
+global using Relecloud.Models.ConcertContext;
+global using Relecloud.Models.Events;
+global using Relecloud.TicketRenderer.Models;
+global using Relecloud.TicketRenderer.Services;
 global using Xunit;
