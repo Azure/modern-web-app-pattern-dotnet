41f240cf908e790aa9aed24ed02503f8f3edb99e
Mike Rousos <mikerou@microsoft.com>
2024-02-20 14:34:21 -0500

    Tweak comment and ensure pre-deploy script has correct permissions

--- infra/scripts/predeploy/call-configure-aca-secrets.sh
+++ infra/scripts/predeploy/call-configure-aca-secrets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script is run by azd pre-deploy hook and is part of the deployment lifecycle run when deploying the code for the Relecloud web app.
+# This script is run by the azd pre-deploy hook and is part of the deployment lifecycle run when deploying the code for the Relecloud web app.
 resourceGroupName=$((azd env get-values --output json) | jq -r .AZURE_RESOURCE_GROUP)
 
 echo "Calling configure-aca-secrets.ps1 for group: '$resourceGroupName'..."
